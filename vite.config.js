14e4c6614935e44b61032d55b7bf5636dce24e79
Kevin Ponce <kevin.poncedeleon@hotmail.com>
2022-08-01 09:07:29 -0300

    deploy.sh added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "https://keviinponcee.github.io/spacex-react-app-master/",
+  base: "https://keviinponcee.github.io/spacex-react-app-master/",
   plugins: [react()]
 })
