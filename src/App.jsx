491df9bac46bc3f8e10c040b5dda0f43254b3bd8
Kevin Ponce <kevin.poncedeleon@hotmail.com>
2022-07-31 23:49:40 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { React } from 'react'
 import { Image } from '@chakra-ui/react'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { LaunchList } from './components/LaunchList'
 import { LaunchDetail } from './components/LaunchDetail'
@@ -9,13 +9,15 @@ import logo from './assets/spacex-logo.png'
 export function App() {
 
   return (
-    <>
+    <div className='App'>
+      <BrowserRouter>
         <Image m={4} src={logo} alt="logo space-x" width={300} />
-      <Routes>
-        <Route path='/' element={<LaunchList />} />
-        <Route path='launch/:launchId' element={<LaunchDetail />} />
-      </Routes>
-    </>
+        <Routes>
+          <Route path='/' element={<LaunchList />} />
+          <Route path='launch/:launchId' element={<LaunchDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 
 }
