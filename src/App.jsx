4756a4c1116125b8c66f1e51860e0812b53a5a40
Kevin Ponce <kevin.poncedeleon@hotmail.com>
2022-07-31 23:24:23 -0300

    - --

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { React } from 'react'
 import { Image } from '@chakra-ui/react'
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import { LaunchList } from './components/LaunchList'
 import { LaunchDetail } from './components/LaunchDetail'
@@ -10,10 +10,10 @@ export function App() {
 
   return (
     <>
-      <Image m={4} src={ logo } alt="logo space-x" width={300} />
       <Routes>
-        <Route path='/' element={<LaunchList/>} />
-        <Route path='launch/:launchId' element={<LaunchDetail/>} />
+        <Image m={4} src={logo} alt="logo space-x" width={300} />
+        <Route path='/' element={<LaunchList />} />
+        <Route path='launch/:launchId' element={<LaunchDetail />} />
       </Routes>
     </>
   )
